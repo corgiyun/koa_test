0c8b76e9256dca75b6569df03620c8204e8be953
lim <569212066@qq.com>
2022-01-10 18:28:59 +0800

    feat: add router

--- app/index.js
+++ app/index.js
@@ -1,17 +1,53 @@
 const Koa = require('koa')
 const Router = require('koa-router')
-const koaBody = require('koa-body')
+const koaBody = require('koa-body');
+const path = require('path');
+const parameter = require('koa-parameter');
+const koaStatic = require('koa-static')
+const mongoose = require('mongoose')
+const routing = require('./routes')
 
 const app = new Koa();
 const router = new Router()
+const { connectionStr } = require('./config')
 
-router.get("/", async (ctx)=> {
-  ctx.body = { message: "Hello World!" }
-})
+mongoose.connect(
+  connectionStr, // 数据库地址
+  { userUnifiedTopology: true, useNewUrlParse: true },
+  ()=> console.log('mongoose 连接成功!')
+)
+mongoose.connection.on('error', console.error)
 
-app.use(router.routes()).use(router.allowedMethods())
+app.use(koaStatic(path.join(__dirname, "public"))) // 静态资源
 
-app.use
+app.use(error({ // 错误处理
+  postFormat: (e, {stack, ...rest})=> {
+    process.env.NODE_ENV = "production" ? rest : {stack, ...rest}
+  }
+}))
 
-app.listen(3001)
+app.use(koaBody({ // 处理 post 请求和图片上传
+  multipart: true, // 支持文件上传
+  encoding: 'gzip',
+  formidable: {
+    uploadDir: path.join(__dirname, 'public/uploads'), // 设置文件上传目录
+    keepExtensions: true, // 保持文件的后缀
+    maxFieldsSize: 2 * 1024 * 1024, // 文件上传大小
+    onFileBegin: (name, file)=> { // 上传文件前的设置
+      console.log(name);
+      console.log(file);
+    }
+  }
+}))
+
+app.use(parameter(app)) // 参数校验
+
+routing(app) // 路由处理
+
+// router.get("/", async (ctx)=> {
+//   ctx.body = { message: "Hello World!" }
+// })
+
+
+app.listen(3001, ()=> console.log("程序启动在3001端口了！"))
 
